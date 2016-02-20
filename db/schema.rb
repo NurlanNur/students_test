46a74eaf3e57f394446887b0324327131fd3a768
Nurlan Nur <nurpashi@gmail.com>
2016-02-20 15:57:12 +0600

    Изменил поле с title на question в моделе Question, closed #15
    затратил 2 мин

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(version: 20160220073704) do
 
   create_table "questions", force: :cascade do |t|
-    t.string   "title"
+    t.text     "question"
     t.integer  "test_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
