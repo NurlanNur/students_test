0668904757ee0cf6382513b7abc8e285d7637a9c
Nurlan Nur <nurpashi@gmail.com>
2016-02-20 16:49:16 +0600

    Создал модель Answer, closed #17
    также создал поля answer:text
    и добавил поле question_id
    
    Затратил 7 мин

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220073704) do
+ActiveRecord::Schema.define(version: 20160220104350) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text     "answer"
+    t.boolean  "is_correct"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
 
   create_table "questions", force: :cascade do |t|
     t.text     "question"
