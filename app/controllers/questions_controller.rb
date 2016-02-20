6b7f77f117b2f09b6d25f22eaffa95506c69e1e7
Nurlan Nur <nurpashi@gmail.com>
2016-02-20 15:33:06 +0600

    Создал экшены и вьюшки для questions_controller, closed #13
    затратил 2 часа

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,65 @@
 class QuestionsController < ApplicationController
+
+  def index
+
+    @test = Test.find(params[:test_id])
+    @questions = Question.where(test_id: @test.id)
+
+  end
+
+
+  # def show
+  #   @test = Test.find(params[:test_id])
+  #   @question = @test.questions(params[:id])
+  #
+  # end
+
+  def new
+    @test = Test.find(params[:test_id])
+    @question = @test.questions.build
+  end
+
+  def edit
+    @test = Test.find(params[:test_id])
+    @question = Question.find(params[:id])
+  end
+
+  def create
+
+    @test = Test.find(params[:test_id])
+    @question = @test.questions.create(question_params)
+    if @question.save
+      flash[:notice] = "Вопрос #{@question.title} успешно создан!"
+      redirect_to test_questions_path(@test)
+    else
+      render 'new'
+    end
+
+  end
+
+  def update
+
+    @test = Test.find(params[:test_id])
+    @question = Question.find(params[:id])
+
+    if @question.update(question_params)
+      flash[:notice] = 'Вопрос успешно обновлен!'
+      redirect_to test_questions_path(@test)
+    else
+      render 'edit'
+    end
+
+  end
+
+  def destroy
+    @test = Test.find(params[:test_id])
+    @question = Question.destroy(params[:id])
+    redirect_to test_questions_path(@test)
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:title, :id, :test_id)
+  end
+
 end
