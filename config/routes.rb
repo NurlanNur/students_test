dd9b673b5334481ead808409cc47ec2a99c18d58
Nurlan Nur <nurpashi@gmail.com>
2016-02-20 16:56:38 +0600

    создал руты для answers, closed #20
    Затратил 1 мин

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :tests do
     resources :questions
+      resources :answers
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
